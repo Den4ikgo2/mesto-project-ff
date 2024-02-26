36267cb45fb3c56bb8c86f038d85fb0372bb8bb1
Podchernyaev Denis <den4ikgo2@yandex.ru>
2024-02-27 00:23:22 +0600

    Выполнил проект

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const container = document.querySelector(".content");
+const placesList = container.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: Функция создания карточки
+/* Функция на добавление карточки */
+const cardCreate = function (cardAdd, cardDel) {
+  cardAdd.forEach(function (element) {
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
-// @todo: Функция удаления карточки
+    cardElement.querySelector(".card__image").src = element.link;
+    cardElement.querySelector(".card__title").textContent = element.name;
 
-// @todo: Вывести карточки на страницу
+    placesList.append(cardElement);
+
+    cardDel();
+  });
+};
+
+/* Колбэк для функции на добавление карточки */
+function cardDelete() {
+  document.querySelector(".places__list").onclick = function (click) {
+    if (click.target.className != "card__delete-button") return;
+    let item = click.target.closest(".card");
+    item.remove();
+  };
+}
+
+/* Добавление карточки */
+cardCreate(initialCards, cardDelete);
