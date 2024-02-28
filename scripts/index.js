bd3117b4fb97cd39fe41e3197d52fd2dbe259d0c
Podchernyaev Denis <den4ikgo2@yandex.ru>
2024-02-28 21:34:36 +0600

    Внес корректировки

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,31 @@
-// @todo: DOM узлы
+
+/* Переменные */
 const container = document.querySelector(".content");
 const placesList = container.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 
-/* Функция на добавление карточки */
-const cardCreate = function (cardAdd, cardDel) {
-  cardAdd.forEach(function (element) {
-    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+/* Функция создания карточки */
+function cardCreate(name, link, alt, cardDel) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const buttonDelete = cardElement.querySelector(".card__delete-button");
 
-    cardElement.querySelector(".card__image").src = element.link;
-    cardElement.querySelector(".card__title").textContent = element.name;
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement.querySelector(".card__image").alt = alt;
 
-    placesList.append(cardElement);
+  buttonDelete.addEventListener("click", cardDel);
 
-    cardDel();
-  });
-};
-
-/* Колбэк для функции на добавление карточки */
-function cardDelete() {
-  document.querySelector(".places__list").onclick = function (click) {
-    if (click.target.className != "card__delete-button") return;
-    let item = click.target.closest(".card");
-    item.remove();
-  };
+  return cardElement;
 }
 
-/* Добавление карточки */
-cardCreate(initialCards, cardDelete);
+/* Функция удаления карточки */
+function deleteCard(event) {
+  const deleteItem = event.target.closest(".card");
+  deleteItem.remove();
+}
+
+/* Добавление карточки в код */
+initialCards.forEach((item) => {
+  const addElement = cardCreate(item.name, item.link, item.alt, deleteCard);
+  placesList.append(addElement);
+});
